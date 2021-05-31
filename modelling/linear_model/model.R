c3c640dfe5b8d443e9eafe5c04353b844a2b1934
Luka van der Plas <l.p.vanderplas@uu.nl>
2021-05-31 19:01:03 +0200

    plots and modelling

--- modelling/linear_model/model.R
+++ modelling/linear_model/model.R
@@ -5,6 +5,7 @@ library(ordinal)
 
 all_results = read.csv("../results/results_with_disagreement.csv")
 
+#===============================================================================================
 #acceptability judgements
 
 simple_model = function(data) {
@@ -19,36 +20,38 @@ simple_model = function(data) {
 m_all = simple_model(all_results)
 summary(m_all)
 
-
-complex_model = function(data) {
+confidence_model = function(data) {
   test_data = subset(data, item_type == "test")
   clmm(response ~ 
-          order * condition * confidence_on_semantic + 
-          order * adj_secondary_type +
+          order * confidence_on_semantic + 
           (1 | participant), 
        test_data)
 }
+
+m_all_confidence = confidence_model(subset(all_results, adj_secondary_type == "scalar" & experiment == 2))
+summary(m_all_confidence)
+
 
 disagreement_model = function(data) {
   test_data = subset(data, item_type == "test")
   clmm(response ~ 
          order * disagreement_on_adj_target +
-         order * adj_secondary_type +
          (1 | participant), 
        test_data)
 }
 
-m_all_disagreement = disagreement_model(all_results)
+m_all_disagreement = disagreement_model(subset(all_results, adj_secondary_type == "scalar" & experiment == 3))
 summary(m_all_disagreement)
 
-# confidence rationgs
+#===============================================================================================
+# confidence ratings
 
 confidence_model = function(data) {
   confidence_data = subset(data, item_type == "confidence")
   clmm(response ~ 
          disagreement_on_adj_target +
          (1|participant),
-       confidence_data) 
+       data = confidence_data) 
 }
 
 m_confidence_2 = confidence_model(subset(all_results, experiment == 2))
