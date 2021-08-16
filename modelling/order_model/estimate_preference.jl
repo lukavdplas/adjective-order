f8621d30e85715567f8092bc866edf6df0764548
Luka van der Plas <lukavdplas@gmail.com>
2021-08-16 16:00:59 +0200

    corpus data

--- modelling/order_model/estimate_preference.jl
+++ modelling/order_model/estimate_preference.jl
@@ -73,6 +73,46 @@ function disagreement_potential(adjective, item)
 	end
 end
 
+# ╔═╡ 76c2ed8b-a354-4c70-819a-410f968d85d0
+md"""### Corpus data
+
+Frequency data from google ngrams (frequencies in year 2019)
+"""
+
+# ╔═╡ 6cac4352-df93-4090-bb6a-d03bff6d6be1
+corpus_data = DataFrame(
+	:adj_target => vcat(repeat(["big"], 6), repeat(["long"], 6)),
+	:adj_secondary => [
+		"expensive", "cheap", "discounted", 
+		"refurbished", "striped", "plastic", 
+		"expensive", "cheap", "discounted", 
+		"metal", "leather", "plastic"],
+	:freq_target_first => [
+		0.0000008678, 0.0000000935, 0.0000000000,
+		0.0000000000, 0.0000003639, 0.0000046894, 
+		0.0000002050, 0.0000000746, 0.0000001048,
+		0.0000090045, 0.0000063455, 0.0000020865],
+	:freq_target_second => [
+		0.0000001391, 0.0000000438, 0.0000000030,
+		0.0000000000, 0.0000000491, 0.0000000253,
+		0.0000007459, 0.0000002318, 0.0000000745,
+		0.0000001710, 0.0000001425, 0.0000000539],
+)
+
+# ╔═╡ bda742af-8a77-4dd5-8def-3066ef3bbbdc
+function corpus_preference_score(row)
+	f_first = row.freq_target_first
+	f_second = row.freq_target_second
+	
+	if (f_first + f_second) < 1e-8
+		missing
+	elseif row.adj_target == "long" && row.adj_secondary == "discounted"
+		missing
+	else
+		(f_first - f_second) / (f_first + f_second)
+	end
+end
+
 # ╔═╡ 5ab54f01-ea3d-40df-bceb-a953d2b9144f
 relative_judgements = let
 	#acceptability judgements
@@ -82,7 +122,7 @@ relative_judgements = let
 	#group by NP (adj1, adj2, noun)
 	groups = groupby(acceptability_results, [:adj_target, :adj_secondary, :scenario])
 	
-	mapreduce(vcat, collect(groups)) do NP_data
+	data = mapreduce(vcat, collect(groups)) do NP_data
 		#collect relative rating per participant
 		per_participant = groupby(NP_data, :participant)
 		
@@ -125,10 +165,43 @@ relative_judgements = let
 			)
 		end
 	end
+	
+	corpus_scores = map(eachrow(data)) do row
+		adj_target = row.adj_target
+		adj_secondary = row.adj_secondary
+		
+		match = first(filter(corpus_data) do row
+				row.adj_target == adj_target && row.adj_secondary == adj_secondary
+			end)
+		
+		corpus_preference = corpus_preference_score(match)
+	end
+	
+	data.corpus_preference = corpus_scores
+	
+	data
 end
 
 # ╔═╡ f1d76242-e88d-4144-a206-912ee6f27c19
 CSV.write(paths[:export], relative_judgements)
+
+# ╔═╡ aa0d3b17-73fd-4c77-8b75-69e4992c5301
+md"A pretty table:"
+
+# ╔═╡ 492c1c5f-ea19-4699-ad35-fb64757ed553
+let
+	data = copy(corpus_data)
+	
+	#convert frequencies from percent to ppb
+	data.freq_target_first = data.freq_target_first * 1e9 / 1e2
+	data.freq_target_second = data.freq_target_second * 1e9 / 1e2
+	
+	#preference scores
+	scores = corpus_preference_score.(eachrow(data))
+	data.preference_score = scores
+	
+	sort(data, [:adj_target, :adj_secondary])
+end
 
 # ╔═╡ ec5983f3-078a-4b7c-a001-ca5aded6659f
 md"""
@@ -136,7 +209,7 @@ md"""
 """
 
 # ╔═╡ 49abd597-ef9a-4be6-8862-010c7e301437
-preference_scale = -5:5
+preference_scale = -4:4
 
 # ╔═╡ adcfea9e-4a77-4046-a12a-3ba652aefbbc
 histogram(
@@ -200,7 +273,7 @@ function plot_preference(x_col, x_scale, data; seriestype = :bar, orientation = 
 			seriestype = seriestype,
 			color = :black,
 			fill = 0,
-			fillcolor = pal[preference + 6];
+			fillcolor = pal[preference + 5];
 			orientation = orientation,
 		)
 	end
@@ -208,19 +281,28 @@ function plot_preference(x_col, x_scale, data; seriestype = :bar, orientation = 
 	plot!(p; kwargs...)
 end
 
+# ╔═╡ 67ecf720-a4bd-41e8-adbe-230b34024fd0
+md"### By mean acceptability"
+
 # ╔═╡ a732fdce-9182-4b64-a1e0-4b8b1b27a7bb
 let
-	mean_values = 1.0:0.5:5.0
+	mean_values = 1.0:0.5:4.0
 	plot_preference(:mean_acceptability, mean_values, relative_judgements,
 		xticks = mean_values, xlabel = "mean acceptability"
 	)
 end
+
+# ╔═╡ 3c64c464-8a4d-411a-9c75-f9c670716003
+md"### By type of secondary adjective"
 
 # ╔═╡ 06e8e659-314b-4d06-8f78-9e2c08c9ff1b
 plot_preference(:adj_secondary_type, ["absolute", "scalar"], relative_judgements,
 	xlabel = "secondary adjective type",
 	plotype = :bar
 )
+
+# ╔═╡ 96eadf44-7d94-46c5-a8ef-6724c927e48d
+md"### By condition"
 
 # ╔═╡ 223480da-0988-436a-a37e-213dee964454
 plot_preference(:condition, ["bimodal", "unimodal"], relative_judgements,
@@ -252,7 +334,7 @@ end
 
 # ╔═╡ 61010c9e-63d7-4999-80ca-7f2eb35fde32
 md"""
-### Relative subjectivity
+### By relative subjectivity
 """
 
 # ╔═╡ b181392f-027c-4817-b52e-af37174af9cb
@@ -284,28 +366,8 @@ if "figures" ∈ readdir(root)
 	md"Figure saved!"
 end
 
-# ╔═╡ 7da08416-d548-4c2c-829a-fde81eda7723
-let
-	preferences = map(subjectivity_scores) do score
-		data = filter(row -> row.relative_subjectivity === score, relative_judgements)
-		data.preference_for_first_order
-	end
-	
-	means = mean.(preferences)
-	errors = std.(preferences)
-
-	p = scatter(subjectivity_scores, means, 
-		yerror = errors,
-		color = 3,
-		marker = :circle, linecolor = :black,
-		xlabel = "relative subjectivity of target",
-		ylabel = "preference for target first",
-		legend = :none
-	)
-	
-	vline!(p, [0], linecolor = :black, linestyle = :dash)
-	hline!(p, [0], linecolor = :black, linestyle = :dash)
-end
+# ╔═╡ ec22046e-d17e-4bf5-9424-62e53276a951
+md"### By adjective pair"
 
 # ╔═╡ 0cebbb6d-3016-4e52-9fa9-3a74a326ae3b
 preference_by_adj_pair = let
@@ -341,6 +403,33 @@ if "figures" ∈ readdir(root)
 	md"Figure saved!"
 end
 
+# ╔═╡ 39ccdc36-7c19-407c-a1ed-be0d424405f3
+md"### By relative frequency in corpus"
+
+# ╔═╡ 65ba0667-cc6d-4d27-9a01-aaf45d9d4b10
+corpus_plot = let
+	data = copy(relative_judgements)
+	data.corpus_preference = round.(data.corpus_preference, digits = 1)
+	
+	corpus_preference_scores = let
+		scores = map(corpus_preference_score, eachrow(corpus_data))
+		rounded_scores = round.(scores, digits = 1)
+		(sort ∘ unique)(rounded_scores)
+		end
+	
+	plot_preference(:corpus_preference, corpus_preference_scores, data,
+		seriestype = :line,
+		xlabel = "relative frequency of target first"
+)
+end
+
+# ╔═╡ c14c94e7-5a62-42ea-8ca9-6fc41c67539b
+if "figures" ∈ readdir(root)
+	savefig(corpus_plot, 
+		paths[:figures] * "preference_by_corpus_freq.pdf")
+	md"Figure saved!"
+end
+
 # ╔═╡ Cell order:
 # ╠═8b8a92c6-d032-11eb-0137-afbbf72b726d
 # ╠═aaa9134a-c7b8-486a-b7f8-62f9450c4fd2
@@ -350,20 +439,31 @@ end
 # ╠═c548a0b4-7a06-4c8c-81b3-0210c8922315
 # ╠═5ab54f01-ea3d-40df-bceb-a953d2b9144f
 # ╠═f1d76242-e88d-4144-a206-912ee6f27c19
+# ╟─76c2ed8b-a354-4c70-819a-410f968d85d0
+# ╠═6cac4352-df93-4090-bb6a-d03bff6d6be1
+# ╠═bda742af-8a77-4dd5-8def-3066ef3bbbdc
+# ╟─aa0d3b17-73fd-4c77-8b75-69e4992c5301
+# ╠═492c1c5f-ea19-4699-ad35-fb64757ed553
 # ╟─ec5983f3-078a-4b7c-a001-ca5aded6659f
 # ╠═49abd597-ef9a-4be6-8862-010c7e301437
 # ╠═adcfea9e-4a77-4046-a12a-3ba652aefbbc
 # ╠═a68bbb19-5b2b-4eaa-8107-3baad14a7450
 # ╠═60eb98a2-c0e9-4002-bddf-914c70235e34
 # ╠═4cbb255c-f33c-4c5a-93ea-50c5f80fb786
+# ╟─67ecf720-a4bd-41e8-adbe-230b34024fd0
 # ╟─a732fdce-9182-4b64-a1e0-4b8b1b27a7bb
+# ╟─3c64c464-8a4d-411a-9c75-f9c670716003
 # ╟─06e8e659-314b-4d06-8f78-9e2c08c9ff1b
+# ╟─96eadf44-7d94-46c5-a8ef-6724c927e48d
 # ╟─223480da-0988-436a-a37e-213dee964454
 # ╟─0d166931-3e23-441b-b985-f6e91a85eb7f
 # ╟─61010c9e-63d7-4999-80ca-7f2eb35fde32
 # ╠═b181392f-027c-4817-b52e-af37174af9cb
 # ╠═a45af2ed-3f57-4176-a807-6db2b29eafcd
 # ╠═cc008e68-6456-4193-ada7-7897162ea111
-# ╠═7da08416-d548-4c2c-829a-fde81eda7723
+# ╟─ec22046e-d17e-4bf5-9424-62e53276a951
 # ╠═0cebbb6d-3016-4e52-9fa9-3a74a326ae3b
 # ╠═79e16b94-b17a-4bd1-b20c-efb631713531
+# ╟─39ccdc36-7c19-407c-a1ed-be0d424405f3
+# ╠═65ba0667-cc6d-4d27-9a01-aaf45d9d4b10
+# ╠═c14c94e7-5a62-42ea-8ca9-6fc41c67539b
--- modelling/order_model/model.R
+++ modelling/order_model/model.R
@@ -8,33 +8,29 @@ all_results$condition = factor(all_results$condition, levels = c("unimodal", "bi
 all_results$preference_for_first_order = as.factor(all_results$preference_for_first_order) #change to factor
 
 #===============================================================================================
-#acceptability judgements
+#preference scores
 
-model = function(data) {
-  clmm(preference_for_first_order ~ 
-         adj_secondary_type +
-         (1 | participant),
-       data = data)
-}
-
-model_subjectivity = function(data) {
-  clmm(preference_for_first_order ~ 
-         relative_subjectivity + 
-         (1 | participant),
-       data = data)
-}
-
+m_secondary_type = clmm(preference_for_first_order ~ 
+                                   adj_secondary_type +
+                                   (1 | participant),
+                                 data = all_results)
+summary(m_secondary_type)
 
 filtered_results = subset(all_results, !is.na(all_results$relative_subjectivity))
 
-m1 = model(filtered_results)
-m2 = model_subjectivity(filtered_results)
+m_secondary_type_filtered = clmm(preference_for_first_order ~ 
+            adj_secondary_type +
+            (1 | participant),
+          data = filtered_results)
+summary(m_secondary_type_filtered)
 
-summary(m1)
-summary(m2)
+m_subjectivity = clmm(preference_for_first_order ~ 
+         relative_subjectivity + 
+         (1 | participant),
+    data = filtered_results)
+summary(m_subjectivity)
 
-anova(m1, m2)
-
+anova(m_secondary_type, m_subjectivity)
 
 best_model = function(data) {
   clmm(preference_for_first_order ~ 
@@ -48,4 +44,18 @@ m3 = best_model(filtered_results)
 summary(m3)
 summary(best_model(all_results))
 
-anova(m1, m3)
+anova(m3, m_subjectivity)
+
+corpus_model = function(data) {
+  clmm(preference_for_first_order ~ 
+         corpus_preference +
+         (1 | participant),
+       data = data)
+}
+
+with_corpus_data = subset(all_results, !is.na(all_results$corpus_preference))
+
+m_corpus = corpus_model(with_corpus_data)
+summary(m_corpus)
+
+summary(best_model(with_corpus_data))
